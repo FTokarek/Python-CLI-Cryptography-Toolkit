c784ce65f65d45f2d3453d71a00fe1fe83d28fa4
Franciszek Tokarek <franciszektokarek@gmail.com>
2025-11-06 15:35:29 +0100

    Add file integrity verification helper

--- modules/hash.py
+++ modules/hash.py
@@ -15,5 +15,13 @@ def hash_file(file_path):
             h.update(chunk)
     return h.hexdigest()
 
+def verify_integrity(file1,file2):
+    hash1 = hash_file(file1)
+    hash2 = hash_file(file2)
+    if hash1 == hash2:
+        print("File is intact")
+    else:
+        print("File is corrupted")
+
 if __name__ == "__main__":
     print("SHA256 hash of the file is: ", hash_file("samples/sample.txt"))
